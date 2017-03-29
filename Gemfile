41c28f37728b63cfa26eadc170733a45219984ee
lucperkins <lucperkins@gmail.com>
2017-03-29 10:19:14 -0700

    remove unused material

--- Gemfile
+++
@@ -1,7 +0,0 @@
-# A sample Gemfile
-source "https://rubygems.org"
-
-#ruby=2.2.2
-#ruby-gemset=reflect-rb
-
-gemspec
--- lib/reflect.rb
+++ lib/reflect.rb
@@ -4,32 +4,8 @@ require 'openssl'
 require 'base64'
 
 require 'reflect/parameter'
-require 'reflect/request_error'
-require 'reflect/client'
 
 module Reflect
-  def self._format_error_message(resp)
-    "An error occured with the request. Status: #{resp.response.code} Error: #{_extract_error_message(resp)}"
-  end
-
-  def self._extract_error_message(resp)
-    begin
-      json = JSON.parse(resp.body)
-      json["error"]
-    rescue JSON::ParserError
-      # TODO: Report this?
-      ""
-    end
-  end
-
-  def self.logger=(logger)
-    @logger = logger
-  end
-
-  def self.logger
-    @logger ||= Logger.new("/dev/null")
-  end
-
   def self.generate_token(secret_key, parameters=[])
     data = parameters.map do |param|
       is_arr = param.value.is_a? Array
